abb4013c28aea5e6a766379b3118f432633bb810
earthscience2 <slhg@hanmail.net>
2024-01-29 03:29:55 +0900

    win

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../styles/home.module.css';
+import styles from '@/styles/home.module.css';
 import { useState } from 'react';
 
 export default function Home() {
