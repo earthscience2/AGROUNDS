ce58e1b72638280fd79563171011a3baed333110
earthscience2 <slhg@hanmail.net>
2024-01-29 03:57:11 +0900

    done

---
+++ frontend/src/pages/signup/google.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>AGROUNDS 구글 회원가입</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ frontend/src/pages/signup/kakao.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>AGROUNDS 카카오 회원가입</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
