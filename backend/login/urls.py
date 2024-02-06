52c19eb16d82dd1a17a32475fbd36468d3cacf53
안의찬 <an@EMacBook-Pro.local>
2024-02-06 16:34:04 +0900

    an - google login

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,9 +1,11 @@
 #필수
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('nickname/', views.nickname.as_view()),
     path('kakao/', views.kakao.as_view()),
     #닉네임 중복 확인/reverse-word/?word=입력단어
+    path('allauth/',include('allauth.urls')),
+    path('google/',views.google.as_view()),
 ]
