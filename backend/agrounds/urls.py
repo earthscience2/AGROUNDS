3da14c9d621eaa9557a51fffecefcf4d55234a82
jayou2323 <jayou1223@gmail.com>
2024-02-09 14:04:14 +0900

    swagger description 수정

--- backend/agrounds/urls.py
+++ backend/agrounds/urls.py
@@ -15,9 +15,9 @@ router = routers.DefaultRouter()
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Statchung API",
+        title="Aground 서비스 api",
         default_version='v1',
-        description="Test description",
+        description="Aground 웹 서비스 api 목록입니다.",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
