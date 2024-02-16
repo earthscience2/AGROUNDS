de93f018591f693e47ac154f6496bc75143297a4
안의찬 <an@EMacBook-Pro.local>
2024-02-16 16:18:49 +0900

    an - team2

--- backend/team/serializers.py
+++ backend/team/serializers.py
@@ -6,47 +6,32 @@ from django.http import JsonResponse
 
 import re
 
-
 class Team_info_Serializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = TeamInfo
+        extra_kwargs = {
+            'team_code' : {'default' : 24}
+        }
+    def create(self, validated_data):
+        instance = super().create(validated_data)
 
-    def create(self,validated_data):
-        instance = self.Meta.model(**validated_data)
+        instance.team_age = 0  # default값
+        instance.team_point = 0  # default값 
         instance.save()
         return instance
     
     def validate(self, data):
-        try:
-            team_host = data.get('team_host')
-            team_name = data.get('team_name')
-            team_player = data.get('team_player', [])
-            team_logo = data.get('team_logo')
-            team_point = data.get('team_point')
-            team_area = data.get('team_area')
-            team_description = data.get('team_description')
-            team_age = data.get('team_age')
-
-            if(
-                not team_host 
-                or not team_name
-                or not team_player
-                or not team_logo
-                or not team_point
-                or not team_area
-                or not team_description
-                or not team_age
-            ):
-                raise serializers.ValidationError("error : 모든 필드는 필수입니다.")
-            
-            seen = set()
-            for player in team_player:
-                if player in seen:
-                    raise serializers.ValidationError("team_player에 중복된 값이 있습니다.")
-                seen.add(player)
-            
-            
-        except Exception as e:  
-            raise serializers.ValidationError('error : ' + str(e))
+        team_player = data.get('team_player', [])  # user_code
+        
+        if not team_player:
+            raise serializers.ValidationError("error : team_player는 필수입니다.")
+        
+        # team_player 중복 확인
+        seen = set()
+        for player in team_player:
+            if player in seen:
+                raise serializers.ValidationError("error : team_player에 중복된 값이 있습니다.")
+            seen.add(player)
+        
         return data
