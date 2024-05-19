65bf8ef0c4c93f275434fb06765ff40060b9d8ee
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-05-19 17:08:35 +0900

    sass 설치, router 설정

--- frontend/aground_demo/src/App.js
+++ frontend/aground_demo/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Route ,Routes, BrowserRouter} from 'react-router-dom';
+import Onboard from './pages/onboard/onboard';
+import SignUp from './pages/sign_up/sign_up';
+import KakaoSignUp from './pages/sign_up/kakao_sign_up';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboard/>}/>
+        <Route path="/signUp" element={<SignUp/>}/>
+        <Route path="/kakao_sign_up" element={<KakaoSignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/aground_demo/src/pages/sign_up/sign_up.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign_up.scss';
+const signUp = () => {
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default signUp;
