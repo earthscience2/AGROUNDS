1096717710ac9f3578ead31477f4bd543b434e12
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-05-26 16:20:33 +0900

    main_page 레이아웃 수정

--- frontend/aground_demo/src/pages/main_page/main_page.js
+++ frontend/aground_demo/src/pages/main_page/main_page.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import UserIcon from '../../assets/user-icon.svg';
+import React, { useEffect, useState } from 'react';
+import UserIcon from '../../assets/soccer-team-logo.png';
 import addEvent from '../../assets/add-event.png';
-import team from '../../assets/team.png';
+import Team from '../../assets/team.png';
+import client from '../../clients';
+import './main_page.scss';
 const MainPage = () => {
+    const [team, setTeam] = useState([])
+    useEffect(() => {
+        client.get('/api/V2match/main/')
+        .then(function(response){
+            setTeam(response.data)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    })
     return (
-        <div>
-            <div>
-                <div>AGROUNDS</div>
-                <img src={addEvent}/>
-                <img src={team}/>
+        <div className='main_page_background'>
+            <div className='main_page_nav_logo'>AGROUNDS</div>
+            <div className='main_page_teamlogobox'>
+                <div className='main_page_teamlogobox_box'>
+                    <div className='main_page_teamlogobox_logobox'><img className='main_page_teamlogobox_logo' src={UserIcon}/></div><div className='main_page_teamlogobox_teamname'>토트넘</div>
+                </div>
+                
+                    <div className='main_page_nav'>
+                    <div className='main_page_title'>경기일정</div>
+                    <img className='main_page_nav_team' src={Team}/>
+                    <img className='main_page_nav_addevent' src={addEvent}/>
+                </div>
             </div>
-            <div>
-                <img src={UserIcon}/><div>teamname</div>
-            </div>
-            <div>
-                <div className='main_page_matchplan_title'>경기일정</div>
-                <div className='main_page_matchplan_teambox'><div className='main_page_matchplan_teamname1'>{myTeam}</div><div className='main_page_matchplan_vs'>VS</div><div className='main_page_matchplan_teamname2'>{teamName}</div></div>
-                <div className='main_page_matchplan_place'>{place}</div>
-                <div className='main_page_matchplan_date'>{date}<div className='main_page_matchplan_datespace'></div>{time}</div>
-            </div>
+            
+            {team.map((match, index) => (
+                <div className='main_page_matchplan' key={index}>
+                    <div className='main_page_matchplan_teambox'><div className='main_page_matchplan_teamname1'>{match.v2_match_home}</div><div className='main_page_matchplan_vs'>VS</div><div className='main_page_matchplan_teamname2'>{match.v2_match_away}</div></div>
+                    <div className='main_page_matchplan_place'>{match.v2_match_location}</div>
+                    <div className='main_page_matchplan_date'><div className='main_page_matchplan_datespace'>{match.v2_match_schedule}</div></div>
+                </div>
+            ))}
+            
 
         </div>
     );
