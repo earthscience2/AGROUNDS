faedd0671f2c8cdfbaf81d0fc794a9ea9b591004
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-05-27 14:49:24 +0900

    teamlist 감독일 경우에만 팀원 삭제기능 구현

--- frontend/aground_demo/src/pages/team/teamlist.js
+++ frontend/aground_demo/src/pages/team/teamlist.js
@@ -4,10 +4,13 @@ import minus from '../../assets/minus.png';
 import client from '../../clients';
 import { useNavigate } from 'react-router-dom';
 import GoBack from '../../assets/go-back-icon.png';
+import classNames from 'classnames';
+
 const TeamList = () => {
     const navigate = useNavigate();
     const [teamList, setTeamList] = useState([]);
     const [teamName, setTeamName] = useState('');
+    
     useEffect(() => {
         client.get('/api/V2team/main/')
         .then(function(response){
@@ -20,18 +23,19 @@ const TeamList = () => {
         })
     }, [teamList])
     
-
+    const teamCode = sessionStorage.getItem('teamcode');
+    const usertype = sessionStorage.getItem('usertype');
     return (
         <div className='teamlist-background'>
             <img className='teamlist_goback_icon' src={GoBack} onClick={() => navigate(-1)} />
             <div className='teamlist-logo'>AGROUNDS</div>
-            <div className='teamlist-title'>{teamName} 팀원 목록</div>
+            <div className='teamlist-title'>팀원 목록</div>
             <div className='teamlist-line'></div>
             <div className='teamlist-largebox'>
                 {teamList.map((player, index) => (
                     <div key={index} className='teamlist-personbox'>
-                        <div className='teamlist-personbox-player'>{player}</div>
-                        {sessionStorage.getItem('usertype') === 0 ? <img className='teamlist-personbox-minus'src={minus}/> : ''}
+                        <div className={classNames(`teamlist-personbox-player ${usertype == 0 ? 'minus' : ''}` )}>{player}</div>
+                        {usertype == 0 ? <img className='teamlist-personbox-minus'src={minus}/> : ''}
                         
                     </div>
                 ))}
