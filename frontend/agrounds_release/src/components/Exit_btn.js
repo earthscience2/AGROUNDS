e229ad41a4eb458ba040a4a6993a3c1bc6053e02
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-11-08 18:01:19 +0900

    선호 포지션 페이지 구현 완료

--- frontend/agrounds_release/src/components/Exit_btn.js
+++ frontend/agrounds_release/src/components/Exit_btn.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import X from '../assets/x.png';
+import { useNavigate } from 'react-router-dom';
 
-const Exit_btn = () => {
+const Exit_btn = ({}) => {
+  const navigate = useNavigate();
   return (
-    <div style={{backgroundColor:'rgba(1,1,1,0.2)', width: '50px', height: '50px', border: '1px solid #6F6F6F', borderRadius: '50%', display: 'flex', justifyContent:'center', alignItems:'center'}}>
+    <div style={{backgroundColor:'rgba(1,1,1,0.4)', width: '50px', height: '50px', border: '1px solid #6F6F6F', borderRadius: '50%', display: 'flex', justifyContent:'center', alignItems:'center'}} onClick={()=> navigate('/extra-info')}>
       <img src={X} style={{width: '25px'}}/>
     </div>
   );
