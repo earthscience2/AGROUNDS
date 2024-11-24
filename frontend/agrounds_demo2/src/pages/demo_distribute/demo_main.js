ca85d01d3c5044e789712fc0006ac930140adcc7
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-11-24 19:00:57 +0900

    오류페이지, 메인페이지 세분화, 속력 가속도 단위 추가

--- frontend/agrounds_demo2/src/pages/demo_distribute/demo_main.js
+++ frontend/agrounds_demo2/src/pages/demo_distribute/demo_main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from '../../components/display/Nav';
 import MainLayout from '../../components/demo/MainLayout';
 import main1 from '../../assets/demo/main1.png';
@@ -7,13 +7,18 @@ import main3 from '../../assets/demo/main3.png';
 import main4 from '../../assets/demo/main4.png';
 import main5 from '../../assets/demo/main5.png';
 import main6 from '../../assets/demo/main6.png';
+import main7 from '../../assets/demo/main7.png';
 import { useNavigate, useLocation } from "react-router-dom";
+import client from '../../clients';
 
 
 const Demo_main = () => {
   const navigate = useNavigate();
-
   const location = useLocation();
+  
+  const [type, setType] = useState();
+  const [error, setError] = useState(null);
+
   const searchParams = new URLSearchParams(location.search);
   
   let userId = searchParams.get('user_id') || '';
@@ -24,33 +29,86 @@ const Demo_main = () => {
     matchCode = 'm_001';
   }
 
-
   sessionStorage.setItem('user_id', userId);
   sessionStorage.setItem('match_code', matchCode);
+
+  const data = {
+    match_code: matchCode
+  }
+
+  useEffect (() => {
+    client.post('/api/test_page/get-match-type/', data)
+    .then((response) => {
+      setType(response.data.match_type);
+      setError(null);
+    })
+    .catch((error) => {
+      setError("매치 타입을 불러오는데 실패했습니다. 다시 시도해주세요."); 
+        console.error(error);
+    })
+  }, [])
+
+
+  const TypeReturn = () => {
+    if (type === 'full') {
+      return (
+        <>
+          <MainLayout imgsrc={main6} bgColor="#616161" onClick={() => navigate('/demo/totalmov')}>확인</MainLayout>
+          <MainLayout imgsrc={main4} bgColor="#616161" onClick={() => window.location.href = 'https://forms.gle/qjFGZjtEWr4FfPe67'}>확인</MainLayout>
+          <MainLayout imgsrc={main7} bgColor="#616161" onClick={() => navigate('/demo/main')}>확인</MainLayout>
+          <MainLayout imgsrc={main5} bgColor="#055540" onClick={() => navigate('/')} >둘러보기</MainLayout>
+        </>
+      )
+    } else if (type === 'full_team') {
+      return (
+        <>
+          <MainLayout imgsrc={main6} bgColor="#616161" onClick={() => navigate('/demo/totalmov')}>확인</MainLayout>
+          <MainLayout imgsrc={main1} bgColor="#616161" onClick={() => navigate('/demo/teamMov')}>확인</MainLayout>
+          <MainLayout imgsrc={main4} bgColor="#616161" onClick={() => window.location.href = 'https://forms.gle/qjFGZjtEWr4FfPe67'}>확인</MainLayout>
+          <MainLayout imgsrc={main7} bgColor="#616161" onClick={() => navigate('/demo/main?user_id=u_001&match_code=m_001')}>확인</MainLayout>
+          <MainLayout imgsrc={main5} bgColor="#055540" onClick={() => navigate('/')} >둘러보기</MainLayout>
+        </>
+      )
+    } else if (type ==='all'){
+      return (
+        <>
+          <MainLayout imgsrc={main6} bgColor="#616161" onClick={() => navigate('/demo/totalmov')}>확인</MainLayout>
+          <MainLayout imgsrc={main1} bgColor="#616161" onClick={() => navigate('/demo/teamMov')}>확인</MainLayout>
+          <MainLayout imgsrc={main2} bgColor="#616161" onClick={() => navigate('/demo/personalMov')}>확인</MainLayout>
+          <MainLayout imgsrc={main3} bgColor="#616161" onClick={() => navigate('/demo/anal')}>확인</MainLayout>
+          <MainLayout imgsrc={main4} bgColor="#616161" onClick={() => window.location.href = 'https://forms.gle/qjFGZjtEWr4FfPe67'}>확인</MainLayout>
+          <MainLayout imgsrc={main5} bgColor="#055540" onClick={() => navigate('/')} >둘러보기</MainLayout>
+      </>
+      )
+    } else {
+      return <p></p>;
+    }
+  }
 
   return (
     <div>
       <Nav />
-      {userId ? 
-      <>
-      <MainLayout imgsrc={main6} bgColor="#616161" onClick={() => navigate('/demo/totalmov')}>확인</MainLayout>
-      <MainLayout imgsrc={main1} bgColor="#616161" onClick={() => navigate('/demo/teamMov')}>확인</MainLayout>
-      <MainLayout imgsrc={main2} bgColor="#616161" onClick={() => navigate('/demo/personalMov')}>확인</MainLayout>
-      <MainLayout imgsrc={main3} bgColor="#616161" onClick={() => navigate('/demo/anal')}>확인</MainLayout>
-      <MainLayout imgsrc={main4} bgColor="#616161" onClick={() => window.location.href = 'https://forms.gle/qjFGZjtEWr4FfPe67'}>확인</MainLayout>
-      <MainLayout imgsrc={main5} bgColor="#055540" onClick={() => navigate('/')} >둘러보기</MainLayout>
-      </>
-      :
-      <>
-      <MainLayout imgsrc={main6} bgColor="#616161" onClick={() => navigate('/demo/totalmov')}>확인</MainLayout>
-      <MainLayout imgsrc={main4} bgColor="#616161" onClick={() => window.location.href = 'https://forms.gle/qjFGZjtEWr4FfPe67'}>확인</MainLayout>
-      <MainLayout imgsrc={main5} bgColor="#055540" onClick={() => navigate('/')} >둘러보기</MainLayout>
-      </>
-      
-      }
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {TypeReturn()}
     </div>
   );
 };
 
 export default Demo_main;
 
+const ErrorMessage = ({ children }) => {
+  return (
+    <div
+      style={{
+        color: "red",
+        backgroundColor: "#ffe5e5",
+        padding: "10px",
+        borderRadius: "5px",
+        margin: "20px 0",
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
