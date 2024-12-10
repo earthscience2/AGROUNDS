095461845b82ae58f80c09dffaabefd6450355bf
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-12-10 16:21:42 +0900

    onboard 페이지 카카오 로고 추가

--- frontend/agrounds_release/src/components/Rec_common_btn.js
+++ frontend/agrounds_release/src/components/Rec_common_btn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import kakao from '../assets/kakao.png';
 
 const Rec_common_btn = ({ backgroundColor='#262626', title="시작하기" ,color='white', onClick }) => {
   const navigate = useNavigate();
@@ -7,6 +8,7 @@ const Rec_common_btn = ({ backgroundColor='#262626', title="시작하기" ,color
   return (
     <div style={{display:'flex',justifyContent:'center', width: '100%'}}>
       <div style={{backgroundColor: backgroundColor, display:'flex', alignItems: 'center' ,justifyContent: 'center', width: '100%', height: '60px', color: color, borderRadius:'1.5vh', fontWeight:'600'}} onClick={onClick}>
+            <img style={{width: '2vh', marginRight: '2vh'}}src={kakao} />
             {title}
       </div>
   </div>
