158cc8e842edbe0dc7ef75b03ecba8d7f7e7943e
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-12-12 21:25:48 +0900

    onboard모달 버그 fix

--- frontend/agrounds_release/src/components/Rec_common_btn.js
+++ frontend/agrounds_release/src/components/Rec_common_btn.js
@@ -7,7 +7,7 @@ const Rec_common_btn = ({ backgroundColor='#262626', title="시작하기" ,color
 
   return (
     <div style={{display:'flex',justifyContent:'center', width: '100%'}}>
-      <div style={{backgroundColor: backgroundColor, display:'flex', alignItems: 'center' ,justifyContent: 'center', width: '100%', height: '60px', color: color, borderRadius:'1.5vh', fontWeight:'600'}} onClick={onClick}>
+      <div style={{backgroundColor: backgroundColor, display:'flex', alignItems: 'center' ,justifyContent: 'center', width: '100%', height: '50px', color: color, borderRadius:'1.5vh', fontWeight:'600'}} onClick={onClick}>
             <img style={{width: '2vh', marginRight: '2vh'}}src={kakao} />
             {title}
       </div>
--- frontend/agrounds_release/src/pages/onboard/js/LoginModal.js
+++ frontend/agrounds_release/src/pages/onboard/js/LoginModal.js
@@ -4,8 +4,8 @@ import '../css/LoginModal.scss';
 const LoginModal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null; 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="modal-overlayl" onClick={onClose}>
+      <div className="modal-contentl" onClick={(e) => e.stopPropagation()}>
         <button className="modal-close" onClick={onClose}>
           &times;
         </button>
