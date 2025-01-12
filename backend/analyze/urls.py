82a35b2b4039ec1e6cd512fbec8e4ea71b81cc41
jayou2323 <jayou1223@gmail.com>
2025-01-12 21:58:49 +0900

    [back] 팀 분석 테스트 api 구현

--- backend/analyze/urls.py
+++ backend/analyze/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('get-analyze-result/', views.getAnalyzeResult.as_view()),
+    path('get-team-analyze-result/', views.getTeamAnalyzeResult.as_view()),
     path('get-overall/', views.getOverall.as_view())
 ]
--- backend/analyze/views.py
+++ backend/analyze/views.py
@@ -35,7 +35,25 @@ class getAnalyzeResult(APIView):
         # 문자열 해시값을 계산하고 0, 1, 2 중 하나로 매핑
         mapped_number = hash(input_string) % 3
         return mapped_number
+
+class getTeamAnalyzeResult(APIView):
+    def post(self, request):
+        data = request.data.copy()
+
+        required_fields = ['match_code', 'team_code']
+        errors = {field: f"{field}는 필수 항목입니다." for field in required_fields if field not in data}
+        if errors:
+            return Response(errors, status=400)
         
+        match_code = data['match_code']
+        team_code = data['team_code']
+        
+        filename = 'team_analyze.json'
+
+        with open(os.path.join(settings.STATIC_ROOT, filename), encoding='utf-8') as file:
+            data = json.load(file)
+            return Response(data)
+
 class getOverall(APIView):
     def post(self, request):
         data = request.data.copy()
