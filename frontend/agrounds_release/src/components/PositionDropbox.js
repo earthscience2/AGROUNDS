10185a4929c5e99adbf8924e2797bad9a2824066
Ntpc_Week2 <munsoyeong12@gmail.com>
2025-02-01 23:42:58 +0900

    myteam dropbox 기능까지 구현

--- frontend/agrounds_release/src/components/PositionDropbox.js
+++ frontend/agrounds_release/src/components/PositionDropbox.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import down from '../assets/down.png';
 
-const positions = ["ALL", "ST", "CM", "CB", "GK"];
+const positions = ["전체", "LWF", "ST", "RWF", "LWM" , "CAM", "RWM", "LM", "CM", "RM", "CDM", "LWB", "RWB" , "LB", "CB", "RB", "GK"];
 
 const PositionDropbox = ({ teamData, setFilteredData }) => {
-  const [selectedPosition, setSelectedPosition] = useState("ALL");
+  const [selectedPosition, setSelectedPosition] = useState("포지션 별");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handlePositionChange = (position) => {
     setSelectedPosition(position);
     setIsDropdownOpen(false);
-    if (position === "ALL") {
+    if (position === "포지션 별" || position === '전체') {
       setFilteredData(teamData);
     } else {
       const filtered = teamData.filter((player) => player.user_position === position);
@@ -21,7 +22,7 @@ const PositionDropbox = ({ teamData, setFilteredData }) => {
   return (
     <PositionDropBoxStyle>
       <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-        {selectedPosition} ▼
+        {selectedPosition} <img className='downbtn' src={down} />
       </button>
       {isDropdownOpen && (
         <Dropdown>
@@ -43,26 +44,48 @@ const PositionDropBoxStyle = styled.div`
   margin: 10px 0;
 
   button {
-    padding: 8px 16px;
-    border: 1px solid #ccc;
-    background-color: #f9f9f9;
-    border-radius: 4px;
     cursor: pointer;
+    border: none;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.6vh;
+    font-weight: 600;
+    color: #8D8D8D;
+    gap: .5vh;
+    padding: 0;
+    .downbtn{
+      height: 1.6vh;
+      
+    }
   }
 `;
 
 const Dropdown = styled.div`
   position: absolute;
-  top: 40px;
+  top: 4vh;
+  right: 0;
   background: white;
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  border: 1px solid white;
+  border-radius: 1vh;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   z-index: 10;
+  width: 15vh;
+  height: 30vh;
+  overflow-y: scroll;
+  font-size: 1.5vh;
+  font-weight: 400;
+  color: #525252;
+  &:last-child{
+    border: none;
+  }
 
   div {
-    padding: 10px;
     cursor: pointer;
+    padding: 2vh 2vh;
+    border-bottom: 1px solid #F2F4F8;
+    
     &:hover {
       background: #f0f0f0;
     }
