d8c07fd961cba8583557189ac87b8f614752e4ca
jayou2323 <jayou1223@gmail.com>
2025-04-03 21:16:52 +0900

    [front] apple 로그인 test

--- frontend/agrounds_release/src/hooks/useAppleRedirectLogin.js
+++ frontend/agrounds_release/src/hooks/useAppleRedirectLogin.js
@@ -10,14 +10,27 @@ export default function useAppleRedirectLogin() {
       window.AppleID.auth.init({
         clientId: process.env.REACT_APP_APPLE_CLIENT_ID,
         scope: 'name email',
-        redirectURI: 'https://agrounds.com/app',
+        redirectURI: 'https://agrounds.com/app/loading-for-login',
         usePopup: true,
       });
 
       window.AppleID.auth.signIn().then(({ authorization }) => {
         const id_token = authorization.id_token;
 
-        window.location.replace(process.env.REACT_APP_BASE_URL + "/api/login/apple/?hostname=" + window.location.hostname);
+        // window.location.replace(
+        //   process.env.REACT_APP_BASE_URL + 
+        //   "/api/login/apple/?hostname=" + 
+        //   window.location.hostname + 
+        //   "&id_token=" + id_token);
+
+        console.log(id_token)
+      })
+      .catch(err => {
+        if (err?.error === 'popup_closed_by_user') {
+          console.log('사용자가 로그인 팝업을 닫았습니다.');
+        } else {
+          console.error('Apple 로그인 에러:', err);
+        }
       });
     } else {
       console.error("AppleID SDK not loaded yet");
